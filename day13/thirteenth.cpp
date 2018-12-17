6a2709c853c9dd5f86064bdf649890b2eb0fbdad
stanislaw kubiak <stahok@gmail.com>
2018-12-18 00:17:46 +0100

    Parsing file done.

--- day13/thirteenth.cpp
+++ day13/thirteenth.cpp
@@ -18,6 +18,19 @@
 #include <deque>
 
 
+enum class tile
+{
+	empty = 0,
+	up_down			/* '|' */,
+	left_right		/* '-' */,
+	curve_left		/* '\' */,
+	curve_right		/* '/' */,
+	intersection	/* '+' */,
+	cart_up			/* '^' */,
+	cart_down		/* 'v' */,
+	cart_left		/* '<' */,
+	cart_right		/* '>' */,
+};
 
 enum class direction
 {
@@ -40,6 +53,54 @@ struct cart
 	direction last_move;
 };
 
+std::array<std::array<tile, 150>, 150> map;
+
+const auto readFile = []
+{
+	std::fstream input("input", std::fstream::in);
+	std::string line;
+	std::size_t line_counter = 0;
+	while (std::getline(input, line))
+	{
+		std::size_t row_counter = 0;
+		for (auto ch : line)
+		{
+			if (ch == '|') map[line_counter][row_counter] = tile::up_down;
+			else if (ch == '-') map[line_counter][row_counter] = tile::left_right;
+			else if (ch == '\\') map[line_counter][row_counter] = tile::curve_left;
+			else if (ch == '/') map[line_counter][row_counter] = tile::curve_right;
+			else if (ch == '+') map[line_counter][row_counter] = tile::intersection;
+			else if (ch == '>') map[line_counter][row_counter] = tile::cart_right;
+			else if (ch == '<') map[line_counter][row_counter] = tile::cart_left;
+			else if (ch == '^') map[line_counter][row_counter] = tile::cart_up;
+			else if (ch == 'v') map[line_counter][row_counter] = tile::cart_down;
+			else map[line_counter][row_counter] = tile::empty;
+			++row_counter;
+		}
+		++line_counter;
+	}
+	input.close();
+};
+
 int main()
 {
+	readFile();
+	for (auto& line : map)
+	{
+		for (auto&i : line)
+		{
+			if (i == tile::up_down) std::cout << '|';
+			else if (i == tile::left_right)		std::cout << '-';
+			else if (i == tile::curve_left)		std::cout << '\\';
+			else if (i == tile::curve_right)	std::cout << '/';
+			else if (i == tile::intersection)	std::cout << '+';
+			else if (i == tile::cart_down)		std::cout << 'v';
+			else if (i == tile::cart_up)		std::cout << '^';
+			else if (i == tile::cart_right)		std::cout << '>';
+			else if (i == tile::cart_left)		std::cout << '<';
+			else								std::cout << ' ';
+		}
+		std::cout << std::endl;
+	}
+	return 0;
 }
