c5e4e7439e1a2ee720f4b07b15e25727c37c766f
stanislaw kubiak <stahok@gmail.com>
2018-12-19 00:43:31 +0100

    Day 13, part 1 almost done...

--- day13/thirteenth.cpp
+++ day13/thirteenth.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include <string>
 #include <algorithm>
 #include <iostream>
@@ -17,7 +16,7 @@
 #include <deque>
 #include <map>
 #include <deque>
-
+#include <queue>
 
 enum class tile
 {
@@ -37,24 +36,42 @@ enum class direction
 	right
 };
 
+enum class turn_orientation
+{
+	left,
+	right,
+	straight
+};
+
 struct cart
 {
 	std::size_t x;
 	std::size_t y;
-	direction current_move;
-	direction last_move;
+	direction orientation;
+	turn_orientation last_move;
 };
 
-constexpr auto size = 13;
+constexpr auto size = 150;
+const auto cart_comparator = [](cart& lhs, cart& rhs)
+{
+	if (lhs.y == rhs.y)
+	{
+		return lhs.x > lhs.x;
+	}
+	return lhs.y > rhs.y;
+};
+
 using map_type = std::array<std::array<tile, size>, size>;
-using cart_type = std::array<std::array<cart, size>, size>;
+using cart_collection = std::vector<cart>;
+using cart_queue = std::priority_queue<cart, std::vector<cart>, decltype(cart_comparator)>;
+
 
 map_type map;
-cart_type positions;
+cart_collection positions;
 
-const auto readFile = []
+const auto readFile = [](map_type& map)
 {
-	std::fstream input("input", std::fstream::in);
+	std::fstream input("C:\\Users\\staho\\projects\\dummy\\x64\\Release\\input", std::fstream::in);
 	std::string line;
 	std::size_t line_counter = 0;
 	while (std::getline(input, line))
@@ -70,22 +87,22 @@ const auto readFile = []
 
 			else if (ch == '>')
 			{
-				positions[line_counter][row_counter] = { line_counter, row_counter, direction::right};
+				positions.push_back({ row_counter, line_counter, direction::right, turn_orientation::left});
 				map[line_counter][row_counter] = tile::left_right;
 			}
 			else if (ch == '<')
 			{
-				positions[line_counter][row_counter] = { line_counter, row_counter, direction::left };
+				positions.push_back({ row_counter, line_counter,  direction::left, turn_orientation::left });
 				map[line_counter][row_counter] = tile::left_right;
 			}
 			else if (ch == '^')
 			{
-				positions[line_counter][row_counter] = { line_counter, row_counter, direction::up };
+				positions.push_back({ row_counter, line_counter,  direction::up, turn_orientation::left });
 				map[line_counter][row_counter] = tile::up_down;
 			}
 			else if (ch == 'v')
 			{
-				positions[line_counter][row_counter] = { line_counter, row_counter, direction::down };
+				positions.push_back({ row_counter, line_counter,  direction::down, turn_orientation::left });
 				map[line_counter][row_counter] = tile::up_down;
 			}
 			else map[line_counter][row_counter] = tile::empty;
@@ -98,35 +115,139 @@ const auto readFile = []
 
 const auto print = [](map_type& map)
 {
-	for (auto& line : map)
+	for (auto i = 0; i < size; ++i)
 	{
-		for (auto&i : line)
+		for (auto j = 0; j < size; ++j)
 		{
-			if (i == tile::up_down) std::cout << '|';
-			else if (i == tile::left_right)		std::cout << '-';
-			else if (i == tile::curve_left)		std::cout << '\\';
-			else if (i == tile::curve_right)	std::cout << '/';
-			else if (i == tile::intersection)	std::cout << '+';
-			else								std::cout << ' ';
+			if (std::any_of(positions.begin(), positions.end(), [i, j](cart& c)
+			{
+				return c.x == j && c.y == i;
+			}))
+			{
+				std::cout << 'X';
+			}
+			else if (map[i][j] == tile::up_down)				std::cout << '|';
+			else if (map[i][j] == tile::left_right)		std::cout << '-';
+			else if (map[i][j] == tile::curve_left)		std::cout << '\\';
+			else if (map[i][j] == tile::curve_right)	std::cout << '/';
+			else if (map[i][j] == tile::intersection)	std::cout << '+';
+			else										std::cout << ' ';
 		}
 		std::cout << std::endl;
 	}
 };
-
-
-auto move_cart = [](cart& c, map_type& map, cart_type& positions)
+const auto getPriorityQueue = [](cart_collection& c)
 {
-	assert(positions[c.x][c.y] == c);
+	cart_queue q(cart_comparator);
+	for (auto e : c)
+		q.push(e);
+	return q;
+};
+
+const auto moveCart = [](cart& c, map_type& map)
+{
+	switch (c.orientation)
+	{
+		case direction::up: c.y -= 1; break;
+		case direction::down: c.y += 1; break;
+		case direction::left: c.x -= 1; break;
+		case direction::right: c.x += 1; break;
+	}
+	const auto tile = map[c.y][c.x];
+	if (tile == tile::intersection)
+	{
+		if (c.last_move == turn_orientation::left)
+		{
+			switch (c.orientation)
+			{
+				case direction::up: c.orientation = direction::left; break;
+				case direction::down: c.orientation = direction::right; break;
+				case direction::left: c.orientation = direction::down; break;
+				case direction::right: c.orientation = direction::up; break;
+			}
+			c.last_move = turn_orientation::straight;
+		}
+		else if (c.last_move == turn_orientation::straight)
+		{
+			c.last_move = turn_orientation::right;
+		}
+		else if (c.last_move == turn_orientation::right)
+		{
+			switch (c.orientation)
+			{
+			case direction::up: c.orientation = direction::right; break;
+			case direction::down: c.orientation = direction::left; break;
+			case direction::left: c.orientation = direction::up; break;
+			case direction::right: c.orientation = direction::down; break;
+			}
+			c.last_move = turn_orientation::left;
+		}
+	}
+	else if (tile == tile::curve_left) // '\'
+	{
+		switch (c.orientation)
+		{
+		case direction::up: c.orientation = direction::left; break;
+		case direction::down: c.orientation = direction::right; break;
+		case direction::left: c.orientation = direction::up; break;
+		case direction::right: c.orientation = direction::down; break;
+		}
+	}
+	else if (tile == tile::curve_right) // '/'
+	{
+		switch (c.orientation)
+		{
+		case direction::up: c.orientation = direction::right; break;
+		case direction::down: c.orientation = direction::left; break;
+		case direction::left: c.orientation = direction::down; break;
+		case direction::right: c.orientation = direction::up; break;
+		}
+	}
+};
+
+const auto colisionDetector = [](cart_collection& v)
+{
+	std::set<std::pair<std::size_t, std::size_t>> temp;
+	auto queue = getPriorityQueue(positions);
+	while (!queue.empty())
+	{
+		auto c = queue.top();
+		queue.pop();
+		if (c.x == 41 && c.y == 17)
+		{
+			std::cout << "ZONK";
+		}
+		if (auto[_, isInserted] = temp.insert(std::make_pair(c.x, c.y)); !isInserted)
+		{
+			std::cout << c.x << " - " << c.y << std::endl;
+			print(map);
+			return true;
+		}
+	}
 	return false;
 };
 
 int main()
 {
-	readFile();
-	print(map);
-	bool isColisionDetected = false;
-	while (!isColisionDetected)
+	readFile(map);
+	//print(map);
+	unsigned counter = 1;
+	while (!colisionDetector(positions))
 	{
+		//std::cout << counter << std::endl;
+		auto queue = getPriorityQueue(positions);
+		positions.clear();
+		while (!queue.empty())
+		{
+			auto c = queue.top();
+			moveCart(c, map);
+			positions.push_back(c);
+			queue.pop();
+		}
+		++counter;
 	}
+
+	//55,40 - NOK
+	//41,17 -> should be like that!
 	return 0;
 }
